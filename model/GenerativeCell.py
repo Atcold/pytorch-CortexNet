af38f95c67d4fa7324362b43e17bdcebfc16bf09
Alfredo Canziani <alfredo.canziani@gmail.com>
2017-02-01 17:43:43 -0500

    Add initial null error and handle None top_down_state

--- model/GenerativeCell.py
+++ model/GenerativeCell.py
@@ -11,32 +11,39 @@ class GenerativeCell(nn.Module):
     Single generative layer
     """
 
-    def __init__(self, input_size, hidden_size):
+    def __init__(self, input_size, hidden_size, error_init_size=None):
         """
         Create a generative cell (bottom_up, r_state) -> error
 
         :param input_size: {'error': error_size, 'up_state': r_state_size}, r_state_size can be 0
         :param hidden_size: int, shooting dimensionality
+        :param error_init_size: tuple, full size of initial (null) error
         """
         super().__init__()
         self.input_size = input_size
         self.hidden_size = hidden_size
-        self.memory = ConvLSTMCell(input_size['error']+input_size['state'], hidden_size)
+        self.error_init_size = error_init_size
+        self.memory = ConvLSTMCell(input_size['error']+input_size['up_state'], hidden_size)
 
-    def forward(self, error, topdown_state, state):
-        return self.memory(torch.cat((error, F.upsample_nearest(topdown_state, scale_factor=2)), 1), state)
+    def forward(self, error, top_down_state, state):
+        if error is None:  # we just started
+            error = Variable(torch.zeros(self.error_init_size))
+        model_input = error
+        if top_down_state is not None:
+            model_input = torch.cat((error, F.upsample_nearest(top_down_state, scale_factor=2)), 1)
+        return self.memory(model_input, state)
 
 
 def test_layer2():
     print('Define model for layer 2')
-    generator = GenerativeCell(input_size={'error': 2*16, 'state': 32}, hidden_size=16)
+    generator = GenerativeCell(input_size={'error': 2*16, 'up_state': 0}, hidden_size=16)
 
     print('Define error and top down state')
     input_error = Variable(torch.randn(1, 2*16, 4, 6))
-    topdown_state = Variable(torch.randn(1, 32, 2, 3))
+    topdown_state = None
 
     print('Input error has size', list(input_error.data.size()))
-    print('Top down state has size', list(topdown_state.data.size()))
+    print('Top down state is None')
 
     print('Forward error and top down state to the model')
     state = None
@@ -50,7 +57,7 @@ def test_layer2():
 
 def test_layer1(topdown_state):
     print('Define model for layer 1')
-    generator = GenerativeCell(input_size={'error': 2*3, 'state': 16}, hidden_size=3)
+    generator = GenerativeCell(input_size={'error': 2*3, 'up_state': 16}, hidden_size=3)
 
     print('Define error and top down state')
     input_error = Variable(torch.randn(1, 2*3, 8, 12))
