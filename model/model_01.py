920912269df6ade0a537b8a8bfdf4c850490ca9e
Alfredo Canziani <alfredo.canziani@gmail.com>
2017-02-16 11:54:29 -0500

    Add training test for model_01 and improve annotation

--- model/model_01.py
+++ model/model_01.py
@@ -29,18 +29,19 @@ class Model01(nn.Module):
         super().__init__()
         self.hidden_layers = len(network_size) - 2
 
+        print('\n{:-^80}'.format(' Building model '))
         print('Hidden layers:', self.hidden_layers)
         print('Net sizing:', network_size)
-        print('Input spatial size:', input_spatial_size)
+        print('Input spatial size: {} x {}'.format(network_size[0], input_spatial_size))
 
         # main auto-encoder blocks
         self.activation_size = [input_spatial_size]
         for layer in range(0, self.hidden_layers):
             # print some annotation when building model
-            print('Layer', str(layer + 1), 40 * '-')
-            print('Bottom size:', self.activation_size[-1])
+            print('{:-<80}'.format('Layer ' + str(layer + 1) + ' '))
+            print('Bottom size: {} x {}'.format(network_size[layer], self.activation_size[-1]))
             self.activation_size.append(tuple(ceil(s / 2) for s in self.activation_size[layer]))
-            print('Top size:', self.activation_size[-1])
+            print('Top size: {} x {}'.format(network_size[layer + 1], self.activation_size[-1]))
 
             # init D (discriminative) blocks
             setattr(self, 'D_' + str(layer + 1), nn.Conv2d(
@@ -55,11 +56,13 @@ class Model01(nn.Module):
                 kernel_size=KERNEL_SIZE, stride=KERNEL_STRIDE, padding=PADDING
             ))
             setattr(self, 'BN_G_' + str(layer + 1), nn.BatchNorm2d(network_size[layer]))
-        print(48 * '-')
 
         # init auxiliary classifier
+        print('{:-<80}'.format('Classifier '))
+        print(network_size[-2], '-->', network_size[-1])
         self.average = nn.AvgPool2d(self.activation_size[-1])
         self.stabiliser = nn.Linear(network_size[-2], network_size[-1])
+        print(80 * '-', '\n')
 
     def forward(self, x, state):
         activation_sizes = [x.size()]  # start from the input
@@ -77,7 +80,6 @@ class Model01(nn.Module):
             if layer:
                 x += residuals[layer - 1]
                 state[layer - 1] = x
-
             x = f.relu(x)
             x = getattr(self, 'BN_G_' + str(layer + 1))(x)
         x_mean = self.average(residuals[-1])
@@ -86,13 +88,12 @@ class Model01(nn.Module):
         return (x, state), (x_mean, video_index)
 
 
-def _test_model_01():
+def _test_model():
     T = 2
     x = torch.rand(T + 1, 1, 3, 4 * 2**3 + 3, 6 * 2**3 + 5)
     K = 10
     y = torch.LongTensor(T, 1).random_(K)
-    model_01 = Model01(network_size=(3, 6, 12, 18, K),
-                       input_spatial_size=x[0].size()[2:])
+    model_01 = Model01(network_size=(3, 6, 12, 18, K), input_spatial_size=x[0].size()[2:])
 
     state = None
     (x_hat, state), (emb, idx) = model_01(V(x[0]), state)
@@ -123,5 +124,53 @@ def _test_model_01():
 
     show_graph(loss_tot)
 
+
+def _test_training():
+    K = 10  # number of training videos
+    network_size = (3, 6, 12, 18, K)
+    T = 6  # sequence length
+    max_epoch = 10  # number of epochs
+    lr = 1e-1  # learning rate
+
+    # set manual seed
+    torch.manual_seed(0)
+
+    print('\n{:-^80}'.format(' Train a ' + str(network_size[:-1]) + ' layer network '))
+    print('Sequence length T:', T)
+    print('Create the input image and target sequences')
+    x = torch.rand(T + 1, 1, 3, 4 * 2**3 + 3, 6 * 2**3 + 5)
+    y = torch.LongTensor(T, 1).random_(K)
+    print('Input has size', tuple(x.size()))
+    print('Target index has size', tuple(y.size()))
+
+    print('Define model')
+    model = Model01(network_size=network_size, input_spatial_size=x[0].size()[2:])
+
+    print('Create a MSE and NLL criterions')
+    mse = nn.MSELoss()
+    nll = nn.CrossEntropyLoss()
+
+    print('Run for', max_epoch, 'iterations')
+    for epoch in range(0, max_epoch):
+        state = None
+        loss = 0
+        for t in range(0, T):
+            (x_hat, state), (emb, idx) = model(V(x[t]), state)
+            loss += mse(x_hat, V(x[t + 1])) + nll(idx, V(y[t]))
+
+        print(' > Epoch {:2d} loss: {:.3f}'.format((epoch + 1), loss.data[0]))
+
+        # zero grad parameters
+        model.zero_grad()
+
+        # compute new grad parameters through time!
+        loss.backward()
+
+        # learning_rate step against the gradient
+        for p in model.parameters():
+            p.data.sub_(p.grad.data * lr)
+
+
 if __name__ == '__main__':
-    _test_model_01()
+    _test_model()
+    _test_training()
