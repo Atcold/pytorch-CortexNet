6a5811f22a764b13b610a3fcab0ddd3a5e33ee40
Alfredo Canziani <alfredo.canziani@gmail.com>
2017-02-20 14:19:03 -0500

    Improve VideoFolder class
    Add static methods and remove external functions
    Add per line comments
    Add docstring to constructor
    Add dynamic deallocation of resources (triggered by video's EOF)
    Get whole small_data_set (sfsdfs-nb.mp4, hand_1-nb.mp4, hand5-nb.mp4)

--- data/VideoFolder.py
+++ data/VideoFolder.py
@@ -17,43 +17,28 @@ from bisect import bisect
 VIDEO_EXTENSIONS = ['.mp4']  # pre-processing outputs MP4s only
 
 
-def _is_video_file(filename):
-    return any(filename.endswith(extension) for extension in VIDEO_EXTENSIONS)
 
 
-def _find_classes(data_path):
-    classes = [d for d in listdir(data_path) if isdir(join(data_path, d))]
-    classes.sort()
-    class_to_idx = {classes[i]: i for i in range(len(classes))}
-    return classes, class_to_idx
 
 
-def _make_data_set(data_path, classes, class_to_idx):
-    videos = list()
-    frames = 0
-    for class_ in tqdm(classes, ncols=80):
-        class_path = join(data_path, class_)
-        for filename in listdir(class_path):
-            if _is_video_file(filename):
-                video_path = join(class_path, filename)
-                video_meta = ffprobe(video_path)
-                start_idx = frames
-                frames += int(video_meta['video'].get('@nb_frames'))
-                item = ((frames - 1, start_idx), (join(class_, filename), class_to_idx[class_]))
-                videos.append(item)
-
-    sleep(0.1)  # allows for progress bar completion
-    return videos, frames
 
 
 class VideoFolder(data.Dataset):
-    def __init__(self, root, batch_size, transform=None, target_transform=None):
-        # TODO: deal with batch_size and fake tot_nb_frames
-        classes, class_to_idx = _find_classes(root)
-        videos, frames = _make_data_set(root, classes, class_to_idx)
+    def __init__(self, root, transform=None, target_transform=None):
+        """
+        Initialise a data.Dataset object for concurrent frame fetching from videos in a directory of folders of videos
+
+        :param root: Data directory (train or validation folders path)
+        :type root: str
+        :param transform: image transform-ing object from ``torchvision.transforms``
+        :type transform: object
+        :param target_transform: label transformation / mapping
+        :type target_transform: object
+        """
+        classes, class_to_idx = self._find_classes(root)
+        videos, frames = self._make_data_set(root, classes, class_to_idx)
 
         self.root = root
-        self.batch_size = batch_size
         self.videos = videos
         self.opened_videos = [[] for _ in videos]
         self.frames = frames
@@ -62,30 +47,14 @@ class VideoFolder(data.Dataset):
         self.transform = transform
         self.target_transform = target_transform
 
-    def _get_frame(self, seek, video_idx):
-
-        opened_video = None
-        if self.opened_videos[video_idx]:
-            # look for matching seek
-            current = self.opened_videos[video_idx]
-            opened_video = next((ov for ov in current if ov[0] + 1 == seek), None)  # unknown warning
-
-        if opened_video is None:
-            video_path = join(self.root, self.videos[video_idx][1][0])
-            video_iter = vreader(video_path)
-            opened_video = [seek, islice(video_iter, seek, None)]
-            self.opened_videos[video_idx].append(opened_video)
-
-        opened_video[0] = seek
-        return next(opened_video[1])
-
     def __getitem__(self, frame_idx):
-        video_idx = bisect(self.videos, ((frame_idx,),))
-        (last, first), (path, target) = self.videos[video_idx]
-        frame = self._get_frame(frame_idx - first, video_idx)
-        if self.transform is not None:
+        frame_idx %= self.frames  # wrap around indexing, if asking too much
+        video_idx = bisect(self.videos, ((frame_idx,),))  # video to which frame_idx belongs
+        (last, first), (path, target) = self.videos[video_idx]  # get video metadata
+        frame = self._get_frame(frame_idx - first, video_idx, frame_idx == last)  # get frame from video
+        if self.transform is not None:  # image processing
             frame = self.transform(frame)
-        if self.target_transform is not None:
+        if self.target_transform is not None:  # target processing
             target = self.target_transform(target)
 
         return frame, target
@@ -93,13 +62,63 @@ class VideoFolder(data.Dataset):
     def __len__(self):
         return self.frames
 
+    def _get_frame(self, seek, video_idx, last):
+
+        opened_video = None  # handle to opened target video
+        if self.opened_videos[video_idx]:  # if handle(s) exists for target video
+            current = self.opened_videos[video_idx]  # get handles list
+            opened_video = next((ov for ov in current if ov[0] == seek), None)  # look for matching seek
+
+        if opened_video is None:  # no (matching) handle found
+            video_path = join(self.root, self.videos[video_idx][1][0])  # build video path
+            video_iter = vreader(video_path)  # get an iterator
+            opened_video = [seek, islice(video_iter, seek, None)]  # go to target seek and create o.v. object
+            self.opened_videos[video_idx].append(opened_video)  # add opened video object to o.v. list
+
+        opened_video[0] = seek + 1  # update seek pointer
+        frame = next(opened_video[1])  # cache output frame
+        if last: self.opened_videos[video_idx].remove(opened_video)  # remove exhausted iterator
+
+        return frame
+
+    def free(self):
+        # TODO: close all video files and empty
+        pass
+
+    @staticmethod
+    def _find_classes(data_path):
+        classes = [d for d in listdir(data_path) if isdir(join(data_path, d))]
+        classes.sort()
+        class_to_idx = {classes[i]: i for i in range(len(classes))}
+        return classes, class_to_idx
+
+    @staticmethod
+    def _make_data_set(data_path, classes, class_to_idx):
+        def _is_video_file(filename_):
+            return any(filename_.endswith(extension) for extension in VIDEO_EXTENSIONS)
+        videos = list()
+        frames = 0
+        for class_ in tqdm(classes, ncols=80):
+            class_path = join(data_path, class_)
+            for filename in listdir(class_path):
+                if _is_video_file(filename):
+                    video_path = join(class_path, filename)
+                    video_meta = ffprobe(video_path)
+                    start_idx = frames
+                    frames += int(video_meta['video'].get('@nb_frames'))
+                    item = ((frames - 1, start_idx), (join(class_, filename), class_to_idx[class_]))
+                    videos.append(item)
+
+        sleep(0.1)  # allows for progress bar completion
+        return videos, frames
+
 
 def test():
     from textwrap import fill, indent
 
     batch_size = 5
 
-    video_data_set = VideoFolder('small_data_set/', batch_size=batch_size)
+    video_data_set = VideoFolder('small_data_set/')
     nb_of_classes = len(video_data_set.classes)
     print('There are', nb_of_classes, 'classes')
     print(indent(fill(' '.join(video_data_set.classes), 77), '   '))
@@ -107,6 +126,8 @@ def test():
     print('Videos in the data set:', *video_data_set.videos, sep='\n')
 
     from PIL import Image
+    import inflect
+    ordinal = inflect.engine().ordinal
 
     # get frames 50 -> 52
     # for i in range(50, 53):
@@ -127,13 +148,18 @@ def test():
     n = ceil(len(video_data_set) / batch_size)
     print('Batch size:', batch_size)
     print('Frames per row:', n)
-    for j in range(3):
-        batch = tuple(video_data_set[i * n + j][0] for i in range(5))
-        batch = concatenate(batch, 0)
+    for big_j in range(0, n, 90):
+        batch = list()
+        for j in range(big_j, big_j + 90):
+            if j >= n: break  # there are no more frames
+            batch.append(tuple(video_data_set[i * n + j][0] for i in range(batch_size)))
+            batch[-1] = concatenate(batch[-1], 0)
+        batch = concatenate(batch, 1)
+        print(batch.shape, 50*'-')
         Image.fromarray(batch).\
-            resize((batch.shape[1]//2, batch.shape[0]//2)).\
-            show(title='batch ' + str(j))
-    print_list(video_data_set.opened_videos)
+            resize((batch.shape[1]//10, batch.shape[0]//10))
+        print(ordinal(big_j // 90 + 1), '90 batches of shape', batch.shape)
+        print_list(video_data_set.opened_videos)
 
     pass
 
