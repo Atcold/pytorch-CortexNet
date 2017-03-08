f153268ac66fa8f4a2cce09565486ef25bc2622d
Alfredo Canziani <alfredo.canziani@gmail.com>
2017-03-08 14:56:19 -0500

    Bugfix new_experiment: add extglob pattern matching

--- new_experiment.sh
+++ new_experiment.sh
@@ -4,8 +4,11 @@
 # It expects a directory / link named "results" in the cwd containing
 # numerically increasing folders with 3 digits
 
+shopt -s extglob  # use extended pattern matching capabilities
+
 old_CLI=$(awk -F': ' '/CLI/{print $2}' last/train.log)
 max_exp=$(ls results/ | tail -1)
+max_exp=${max_exp##*(0)}  # remove possible leading 0s
 dst_path=$(printf "results/%03d" "$((++max_exp))")
 
 echo -n " > Creating folder: "
